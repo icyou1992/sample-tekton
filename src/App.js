674883828df6e6ea073f8e4bbc2d67dffa61e11d
icyou1992 <icyou1992@gmail.com>
2023-09-16 16:16:32 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>PFe DEMO APP</code> 
+        </p>
+        <div>asdasd</div>
+        <div>qwerqwer</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
