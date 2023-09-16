712de82211e5219642a42fa6c4b000d551ce9af5
icyou1992 <icyou1992@gmail.com>
2023-09-16 22:27:10 +0900

    update src

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
+import React, { useState, useEffect } from 'react'
 import logo from './logo.png';
 import arch from './arch.jpg';
 import './App.css';
 
 function App() {
+  const items = useState([]);
+  const padding = 8;
+
+  const styles = {
+    app: {
+      display: 'flex',
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: padding,
+    },
+    left: {
+      display: 'flex',
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: 'center',
+      padding: padding,
+    },
+    right: {
+      display: 'flex',
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: 'center',
+      padding: padding,
+    },
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <div>
+        <div style={styles.left}>
           <img src={logo} className="App-logo" alt="logo" />
           <img src={arch} />
         </div>
-        <div>
+        <div style={styles.right}>
           <p>
             <code>PFe DEMO APP</code> 
           </p>
-          <div>asdasd</div>
+          <div style={styles.contents}>
+            {items.map((item, index) => (
+              <div key={index}>{item}</div>
+            ))}
+          </div>
           <div>qwerqwer</div>
         </div>
       </header>
@@ -22,26 +55,5 @@ function App() {
   );
 }
 
-const styles = {
-  app: {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  left: {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  right: {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-}
 
 export default App;
