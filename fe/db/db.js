1f4e477df0389e9dbcc5d1d2742e7904061b2a3d
icyou1992 <icyou1992@gmail.com>
2023-09-17 19:53:00 +0900

    separate fe, be

--- fe/db/db.js
+++ fe/db/db.js
@@ -1,15 +1,21 @@
-import express from "express"
-import axios from "axios"
-
-
 const mysql = require('mysql');	// express 를 가져온 것 처럼 mysql 도 가져오자.
+const port = 3306;
 
 connection = mysql.createConnection({
     host:'db-pfe-dev.cw4iktllotnd.ap-northeast-2.rds.amazonaws.com',
-    port:'3306',
+    port: port,
     user:'pfe',
     password:'test123!',
     database:'demo',
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+  
+    console.log('Connected to database.');
 });
 
 module.exports = db;
