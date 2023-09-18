9e327ff62f4133eeb27ec2eef4f5497614d9d763
icyou <ic.jeon@lgcns.com>
2023-09-18 13:03:28 +0900

    end?

---
+++ be/config.js
@@ -0,0 +1,9 @@
+const dbConfig = {
+  host: 'db-pfe-dev.cw4iktllotnd.ap-northeast-2.rds.amazonaws.com',
+  user: 'pfe',
+  password: 'testtest',
+  database: 'demo',
+  port: 3306,
+}
+
+module.exports = dbConfig
