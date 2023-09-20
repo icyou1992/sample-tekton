86145388e7dd215813b3f5fdd470ec827d2b4289
icyou <66767684+icyou1992@users.noreply.github.com>
2023-09-21 07:49:26 +0900

    Delete be/config.js

--- be/config.js
+++
@@ -1,9 +0,0 @@
-const dbConfig = {
-  host: 'db-pfe-dev.cw4iktllotnd.ap-northeast-2.rds.amazonaws.com',
-  user: 'pfe',
-  password: 'testtest',
-  database: 'demo',
-  port: 3306,
-}
-
-module.exports = dbConfig
